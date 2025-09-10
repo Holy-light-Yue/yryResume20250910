2307694671bb3594194e50fb2411b3ae3557580b
Holy-light-Yue <3197228957@qq.com>
2025-09-10 20:50:38 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// 鼠标移动特效
-const body = document.querySelector('body');
-body.addEventListener('mousemove', (e) => {
-    const x = e.clientX / window.innerWidth;
-    const y = e.clientY / window.innerHeight;
-    body.style.backgroundPosition = `${x * 100}% ${y * 100}%`;
-});
-
-console.log('简历页面加载完成');
